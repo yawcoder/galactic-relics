5480d36b8f56fb1c8ce2b85770d9178f25a05b98
yawcoder <isaac16anim@gmail.com>
2024-08-18 22:05:24 +0000

    employees page added

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function Employee() {
+    const idObject = useParams();
+    const id = idObject.id
+
+    const [employee, setEmployee] = useState([])
+
+    function getEmployee(){
+        Axios.get(`https://bcgalacticgadgetsapi-production.up.railway.app/api/Employees/${id}`).then((response) => {
+            setEmployee(response.data);
+            // console.log(response.data);
+        })
+    }
+
+    useEffect(() => {
+        getEmployee();
+    }, [])
+
+
   return (
-    <div>Employee</div>
+    <div>
+        <p>{employee.firstName}</p>
+        <p>{employee.lastName}</p>
+        <p>{employee.emailAddress}</p>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,13 +1,14 @@
-import Axios from "axios"
-import { useEffect, useState } from "react"
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Employees() {
     const [employees, setEmployees] = useState([])
 
     function getEmployees(){
         Axios.get("https://bcgalacticgadgetsapi-production.up.railway.app/api/Employees?page=1&pageSize=100").then((response) => {
-            setEmployees(response.data);
-            console.log(response.data);
+            setEmployees(response.data.data);
+            // console.log(response.data.data);
         })
     }
 
@@ -16,7 +17,11 @@ function Employees() {
     }, [])
 
   return (
-    <div>Allemployees</div>
+    <div>
+      {employees.map((employee) => {
+        return <Link to={`/employee/${employee.id}`} key={employee.id}><span>{employee.firstName}</span> <span>{employee.lastName}</span></Link>
+      })}
+    </div>
   )
 }
 
