87564f75ed61d3118bffd95e1c2fa972aff58f5f
yawcoder <isaac16anim@gmail.com>
2024-08-21 16:25:28 +0000

    filter orders by employee added. Next: getOrder() triggers and returns all orders when 'next' or 'prev' button is clicked

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,19 +5,46 @@ import { Link } from 'react-router-dom';
 function Orders() {
     const [orders, setOrders] = useState([]);
     const [pageNumber, setPageNumber] = useState(1);
+    const [employees, setEmployees] = useState([]);
+    const [selectedEmployee, setSelectedEmployee] = useState("All Orders");
 
     function getOrders(){
         axios.get(`https://bcgalacticgadgetsapi-production.up.railway.app/api/Orders?page=${pageNumber}&pageSize=10`).then((response) => {
-            console.log(response.data.data);
+            // console.log(response.data.data);
             setOrders(response.data.data);
         }).catch((error) => {
             console.log(error.response.status)
         })
     }
 
+    function getEmployees(){
+        axios.get("https://bcgalacticgadgetsapi-production.up.railway.app/api/Employees?page=1&pageSize=100").then((response) => {
+            setEmployees(response.data.data);
+            // console.log(response.data.data);
+        }).catch(function(error){
+          console.log(error.response.status)
+        })
+    }
+
+    function handleChange(event){
+        setSelectedEmployee(event.target.value);
+    }
+
     useEffect(() => {
         getOrders();
-    }, [pageNumber])
+        getEmployees();
+    }, [pageNumber]);
+
+    useEffect(() => {
+        if(selectedEmployee !== "All Orders"){
+            axios.get(`https://bcgalacticgadgetsapi-production.up.railway.app/api/Orders?employeeId=${selectedEmployee}&page=1&pageSize=100`).then((response) => {
+                // console.log(response.data.data);
+                setOrders(response.data.data);
+            }).catch((error) => {
+                console.log(error.response.data);
+            })
+        }
+    }, [selectedEmployee])
 
     function nextPage(){
         pageNumber > 9 ? setPageNumber(1) : setPageNumber(pageNumber + 1);
@@ -31,7 +58,12 @@ function Orders() {
   return (
     <div>
         <div>
-            
+            <select value={selectedEmployee} onChange={handleChange}>
+                <option key="All-orders" value="All Orders">--Select--</option>
+                {employees.map((employee) => {
+                    return <option key={employee.id} value={employee.id} >{employee.firstName} {employee.lastName}</option>
+                })}
+            </select>
             {orders.map((order) => {
                 return (
                     <div key={order.id}>
