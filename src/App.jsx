59fe72171efa82d6defb3669c33e4cffad504f3f
yawcoder <isaac16anim@gmail.com>
2024-08-28 09:02:24 +0000

    navbar done. Next: styling Orders page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import Employees from './pages/Employees';
 import Employee from './pages/Employee';
 import Orders from './pages/Orders';
 import Order from './pages/Order';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
+    
     <Router>
+      <Navbar />
+      
       <Routes>
         <Route path='/categories' element={<Categories />}/>
         <Route path='/all-products' element={<Allproducts />}/>
